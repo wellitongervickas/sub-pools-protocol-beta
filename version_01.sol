2bdf232bb3df2ae33f34eefce1737497e2df674d
Welliton Gervickas <wellitogervickas@gmail.com>
2023-06-06 17:11:43 -0300

    Update version_01.sol

--- version_01.sol
+++ version_01.sol
@@ -5,36 +5,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SubPool is Ownable {
     address public manager;
-    mapping(address => SubPool) public subPools;
+    uint256 public nextSubPoolID = 0;
+    mapping(uint256 => address) public subPools;
 
     constructor(address _manager) {
         manager = _manager;
     }
 
-    function join(address _subPoolAddress) external onlyOwner {
-        SubPool subPool = SubPool(_subPoolAddress);
-        subPools[_subPoolAddress] = subPool;
+    function join(address _subPoolAddress, uint256 subPoolId) external onlyOwner {
+        subPools[subPoolId] = _subPoolAddress;
+        nextSubPoolID++;
     }
 }
 
 contract RouterSubPool {
     event Created(address indexed subPool);
-    event Joined(address indexed parentSubPool, address indexed subPool);
+    event Joined(address indexed parentSubPool, address indexed subPool, uint256 indexed subPoolId);
 
     function create() external returns (address) {
         SubPool subPool = new SubPool(msg.sender);
 
         emit Created(address(subPool));
-
         return address(subPool);
     }
 
-    function join(address _parentSubPool, address _subPoolAddress) external returns (bool) {
-        SubPool parentSubPool = SubPool(_parentSubPool);
-        parentSubPool.join(_subPoolAddress);
+    function join(address _parentSubPoolAddress, address _subPoolAddress) external returns (bool) {
+        SubPool _parentSubPool = SubPool(_parentSubPoolAddress);
+        uint256 _currentID = _parentSubPool.nextSubPoolID();
 
-        emit Joined(_parentSubPool, _subPoolAddress);
+        _parentSubPool.join(_subPoolAddress, _currentID);
 
+        emit Joined(_parentSubPoolAddress, _subPoolAddress, _currentID);
         return true;
     }
 }
