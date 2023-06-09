ab490ce4d0f01bbf02933dd1558c06510c3f14e2
welliton gervickas <wellitogervickas@gmail.com>
2023-06-09 17:49:28 -0300

    feat: hardhat settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-solhint";
-
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-solhint'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+}
 
-export default config;
+export default config
