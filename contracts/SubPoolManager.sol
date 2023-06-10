b3b293373d10e67a54bec7e6a4c29a6da81204df
welliton gervickas <wellitogervickas@gmail.com>
2023-06-10 12:55:26 -0300

    fix: adding comments

--- contracts/SubPoolManager.sol
+++ contracts/SubPoolManager.sol
@@ -8,50 +8,82 @@ import './lib/SubPoolLib.sol';
 contract SubPoolManager {
     using SubPoolLib for SubPoolLib.SubPoolInfo;
 
-    uint256 public nextSubPoolID = 1;
-
+    uint256 public nextMainPoolID = 1;
     mapping(address => SubPoolLib.SubPoolInfo) public subPools;
 
-    error NotAllowed();
+    error NotSubPool();
+
+    /**
+     * @notice Create a new main subpool
+     * @param _amount The amount of the initial deposit
+     * @return The address of the new main subpool
+     */
 
     function createMain(uint256 _amount) external returns (address) {
         SubPoolNode _subPool = new SubPoolNode();
         address _subPoolAddress = address(_subPool);
 
-        subPools[_subPoolAddress] = SubPoolLib.SubPoolInfo({id: nextSubPoolID, initialBalance: 0, balance: 0});
+        subPools[_subPoolAddress] = SubPoolLib.SubPoolInfo({id: nextMainPoolID, initialBalance: 0, balance: 0});
 
         _subPool.setParentSubPool(address(this));
         _initialDeposit(_subPoolAddress, _amount);
-        nextSubPoolID++;
+
+        nextMainPoolID++;
 
         return _subPoolAddress;
     }
 
+    /**
+     * @notice Create a new subpool node
+     * @param _parentSubPoolAddress The address of the parent subpool
+     * @param _amount The amount of the initial deposit
+     * @return The address of the new subpool node
+     * @return The ID of the new subpool node
+     */
     function createNode(address _parentSubPoolAddress, uint256 _amount) external returns (address, uint256) {
         SubPoolNode _subPool = new SubPoolNode();
+
         address _subPoolAddress = address(_subPool);
         uint256 _subPoolId = _join(_parentSubPoolAddress, _subPoolAddress, _amount);
 
         return (_subPoolAddress, _subPoolId);
     }
 
+    /**
+     * @notice Join a subpool node
+     * @param _parentSubPoolAddress The address of the parent subpool
+     * @param _subPoolAddress The address of the subpool node
+     * @param _amount The amount of the initial deposit
+     * @return The ID of the new subpool node
+     */
     function _join(address _parentSubPoolAddress, address _subPoolAddress, uint256 _amount) internal returns (uint256) {
         SubPoolNode _parentSubPool = SubPoolNode(_parentSubPoolAddress);
         SubPoolNode _subPool = SubPoolNode(_subPoolAddress);
 
         uint256 _subPoolId = _parentSubPool.join(_subPoolAddress, _amount);
+
         _subPool.setParentSubPool(_parentSubPoolAddress);
 
         return _subPoolId;
     }
 
+    /**
+     * @notice Initial deposit of a main sub pool
+     * @param _subPoolAddress The address of the main sub pool
+     * @param _amount The amount of the initial deposit
+     */
     function _initialDeposit(address _subPoolAddress, uint256 _amount) internal {
         subPools[_subPoolAddress]._initialDeposit(_amount);
     }
 
+    /**
+     * @notice Additional deposit of a sub pool
+     * @param _subPoolAddress The address of the sub pool
+     * @param _amount The amount of the additional deposit
+     */
     function additionalDeposit(address _subPoolAddress, uint256 _amount) external {
         bool isNode = subPools[_subPoolAddress]._checkSenderIsNode(msg.sender, _subPoolAddress);
-        if (!isNode) revert NotAllowed();
+        if (!isNode) revert NotSubPool();
 
         subPools[_subPoolAddress]._additionalDeposit(_amount);
     }
--- contracts/SubPoolNode.sol
+++ contracts/SubPoolNode.sol
@@ -9,18 +9,27 @@ contract SubPoolNode is Ownable {
 
     address public parentSubPool;
     uint256 public nextSubPoolID = 1;
-
     mapping(address => SubPoolLib.SubPoolInfo) public subPools;
 
     error ParentNotFound();
     error NotAllowed();
 
+    /**
+     * @notice Set the parent subpool
+     * @param _parentSubPool The address of the parent subpool
+     */
     function setParentSubPool(address _parentSubPool) external onlyOwner {
         parentSubPool = _parentSubPool;
     }
 
+    /**
+     * @notice Join a subpool
+     * @param _subPoolAddress The address of the subpool
+     * @param _amount The amount of the initial deposit
+     * @return The ID of the subpool
+     */
     function join(address _subPoolAddress, uint256 _amount) external onlyOwner returns (uint256) {
-        if (_checkEmptyParentSubPool()) revert ParentNotFound();
+        if (_checkEmptyParent()) revert ParentNotFound();
 
         subPools[_subPoolAddress] = SubPoolLib.SubPoolInfo({id: nextSubPoolID, initialBalance: 0, balance: 0});
 
@@ -30,11 +39,21 @@ contract SubPoolNode is Ownable {
         return subPools[_subPoolAddress].id;
     }
 
+    /**
+     * @notice update the initial deposit and balance on the parent subpool
+     * @param _subPoolAddress The address of the subpool
+     * @param _amount The amount of the initial deposit
+     */
     function _initialDeposit(address _subPoolAddress, uint256 _amount) internal {
         subPools[_subPoolAddress]._initialDeposit(_amount);
         _updateParentBalance(_amount);
     }
 
+    /**
+     * @notice Additional deposit of a sub pool
+     * @param _subPoolAddress The address of the sub pool
+     * @param _amount The amount of the additional deposit
+     */
     function additionalDeposit(address _subPoolAddress, uint256 _amount) external {
         bool _isNode = subPools[_subPoolAddress]._checkSenderIsNode(msg.sender, _subPoolAddress);
         if (!_isNode) revert NotAllowed();
@@ -43,12 +62,19 @@ contract SubPoolNode is Ownable {
         _updateParentBalance(_amount);
     }
 
+    /**
+     * @notice Update the balance of subpool itself on the parent subpool
+     * @param _amount The amount of additional deposit
+     */
     function _updateParentBalance(uint256 _amount) internal {
         SubPoolNode _parentSubPool = SubPoolNode(parentSubPool);
         _parentSubPool.additionalDeposit(address(this), _amount);
     }
 
-    function _checkEmptyParentSubPool() internal view returns (bool) {
+    /**
+     * @notice Check if the parent subpool is empty
+     */
+    function _checkEmptyParent() internal view returns (bool) {
         return parentSubPool == address(0);
     }
 }
