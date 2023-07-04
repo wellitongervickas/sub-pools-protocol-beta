9f16ff18455365a3680e92ab276aec9822d673b5
welliton gervickas <wellitogervickas@gmail.com>
2023-07-04 19:44:55 -0300

    fix: tests

--- test/Validations/Router.spec.ts
+++ test/Validations/Router.spec.ts
@@ -12,113 +12,111 @@ import {
 } from '../fixtures'
 
 describe('SubPoolRouter', () => {
-  describe('Validations', () => {
-    describe('Additional deposit', () => {
-      it('should revert if try to call additional deposit without being the manager', async function () {
-        const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
-        const [, anyEntity] = accounts
-        const nodeAddress = await subPoolNode.getAddress()
-        const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+  describe('Additional deposit', () => {
+    it('should revert if try to call additional deposit without being the manager', async function () {
+      const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
+      const [, anyEntity] = accounts
+      const nodeAddress = await subPoolNode.getAddress()
+      const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
 
-        await expect(
-          anyEntityRouterInstance.additionalDeposit(nodeAddress, ethers.toBigInt(100))
-        ).to.be.revertedWithCustomError(subPoolRouter, 'NotNodeManager()')
-      })
+      await expect(
+        anyEntityRouterInstance.additionalDeposit(nodeAddress, ethers.toBigInt(100))
+      ).to.be.revertedWithCustomError(subPoolRouter, 'NotNodeManager()')
+    })
+  })
+
+  describe('Deposit', () => {
+    it('should revert when try to call deposit if sender is not node', async function () {
+      const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
+      const [, anyEntity] = accounts
+      const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+
+      await expect(anyEntityRouterInstance.deposit(ethers.toBigInt(100))).to.be.revertedWithCustomError(
+        subPoolRouter,
+        'NotAllowed()'
+      )
+    })
+  })
+
+  describe('Withdraw Balance', () => {
+    it('should revert if try to call withdraw balance without being the manager', async function () {
+      const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
+      const [, anyEntity] = accounts
+      const nodeAddress = await subPoolNode.getAddress()
+      const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+
+      await expect(
+        anyEntityRouterInstance.withdrawBalance(nodeAddress, ethers.toBigInt(100))
+      ).to.be.revertedWithCustomError(subPoolRouter, 'NotNodeManager()')
     })
 
-    describe('Deposit', () => {
-      it('should revert when try to call deposit if sender is not node', async function () {
-        const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
-        const [, anyEntity] = accounts
-        const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+    it('should revert if try to call withdraw initial balance without being the manager', async function () {
+      const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
+      const [, anyEntity] = accounts
+      const nodeAddress = await subPoolNode.getAddress()
+      const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
 
-        await expect(anyEntityRouterInstance.deposit(ethers.toBigInt(100))).to.be.revertedWithCustomError(
-          subPoolRouter,
-          'NotAllowed()'
-        )
-      })
+      await expect(
+        anyEntityRouterInstance.withdrawInitialBalance(nodeAddress, ethers.toBigInt(100))
+      ).to.be.revertedWithCustomError(subPoolRouter, 'NotNodeManager()')
     })
 
-    describe('Withdraw Balance', () => {
-      it('should revert if try to call withdraw balance without being the manager', async function () {
-        const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
-        const [, anyEntity] = accounts
-        const nodeAddress = await subPoolNode.getAddress()
-        const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+    it('should revert when try to call withdraw if sender is not node', async function () {
+      const { subPoolRouter, accounts } = await loadFixture(deployRoutedNodeFixture)
+      const [, anyEntity] = accounts
+      const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
 
-        await expect(
-          anyEntityRouterInstance.withdrawBalance(nodeAddress, ethers.toBigInt(100))
-        ).to.be.revertedWithCustomError(subPoolRouter, 'NotNodeManager()')
-      })
+      await expect(anyEntityRouterInstance.withdraw(ethers.toBigInt(100))).to.be.revertedWithCustomError(
+        subPoolRouter,
+        'NotAllowed()'
+      )
+    })
 
-      it('should revert if try to call withdraw initial balance without being the manager', async function () {
-        const { subPoolRouter, subPoolNode, accounts } = await loadFixture(deployRoutedNodeFixture)
-        const [, anyEntity] = accounts
-        const nodeAddress = await subPoolNode.getAddress()
-        const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+    it('should revert when try to call cashback if sender is not node', async function () {
+      const { subPoolRouter, accounts } = await loadFixture(deployRoutedNodeFixture)
+      const [, anyEntity] = accounts
+      const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
 
-        await expect(
-          anyEntityRouterInstance.withdrawInitialBalance(nodeAddress, ethers.toBigInt(100))
-        ).to.be.revertedWithCustomError(subPoolRouter, 'NotNodeManager()')
-      })
+      await expect(anyEntityRouterInstance.cashback(ethers.toBigInt(100))).to.be.revertedWithCustomError(
+        subPoolRouter,
+        'NotAllowed()'
+      )
+    })
 
-      it('should revert when try to call withdraw if sender is not node', async function () {
-        const { subPoolRouter, accounts } = await loadFixture(deployRoutedNodeFixture)
-        const [, anyEntity] = accounts
-        const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+    it('should revert if try to withdraw initial balance in locked period', async function () {
+      const { subPoolRouter, accounts } = await loadFixture(deployRouterFixture)
 
-        await expect(anyEntityRouterInstance.withdraw(ethers.toBigInt(100))).to.be.revertedWithCustomError(
-          subPoolRouter,
-          'NotAllowed()'
-        )
-      })
+      const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
+      const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS
 
-      it('should revert when try to call cashback if sender is not node', async function () {
-        const { subPoolRouter, accounts } = await loadFixture(deployRoutedNodeFixture)
-        const [, anyEntity] = accounts
-        const anyEntityRouterInstance = subPoolRouter.connect(anyEntity) as any
+      const amount = ethers.toBigInt(1000)
+      const [, invited] = accounts
 
-        await expect(anyEntityRouterInstance.cashback(ethers.toBigInt(100))).to.be.revertedWithCustomError(
-          subPoolRouter,
-          'NotAllowed()'
-        )
-      })
+      const tx = await subPoolRouter.create(
+        amount,
+        DEFAULT_FEES_FRACTION,
+        [invited.address],
+        unlockTime,
+        DEFAULT_REQUIRED_INITIAL_AMOUNT,
+        DEFAULT_MAX_ADDITIONAL_AMOUNT
+      )
+      let receipt = await tx.wait()
+      const [subPoolAddress] = receipt.logs[3].args
 
-      it('should revert if try to withdraw initial balance in locked period', async function () {
-        const { subPoolRouter, accounts } = await loadFixture(deployRouterFixture)
+      const invitedRouterInstance = subPoolRouter.connect(invited) as any
+      await invitedRouterInstance.join(
+        subPoolAddress,
+        amount,
+        DEFAULT_FEES_FRACTION,
+        [],
+        unlockTime,
+        DEFAULT_REQUIRED_INITIAL_AMOUNT,
+        DEFAULT_MAX_ADDITIONAL_AMOUNT
+      )
 
-        const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
-        const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS
-
-        const amount = ethers.toBigInt(1000)
-        const [, invited] = accounts
-
-        const tx = await subPoolRouter.create(
-          amount,
-          DEFAULT_FEES_FRACTION,
-          [invited.address],
-          unlockTime,
-          DEFAULT_REQUIRED_INITIAL_AMOUNT,
-          DEFAULT_MAX_ADDITIONAL_AMOUNT
-        )
-        let receipt = await tx.wait()
-        const [subPoolAddress] = receipt.logs[3].args
-
-        const invitedRouterInstance = subPoolRouter.connect(invited) as any
-        await invitedRouterInstance.join(
-          subPoolAddress,
-          amount,
-          DEFAULT_FEES_FRACTION,
-          [],
-          unlockTime,
-          DEFAULT_REQUIRED_INITIAL_AMOUNT,
-          DEFAULT_MAX_ADDITIONAL_AMOUNT
-        )
-
-        await expect(
-          subPoolRouter.withdrawInitialBalance(subPoolAddress, ethers.toBigInt(100))
-        ).to.be.revertedWithCustomError(subPoolRouter, 'LockPeriod()')
-      })
+      await expect(
+        subPoolRouter.withdrawInitialBalance(subPoolAddress, ethers.toBigInt(100))
+      ).to.be.revertedWithCustomError(subPoolRouter, 'LockPeriod()')
     })
   })
 })
