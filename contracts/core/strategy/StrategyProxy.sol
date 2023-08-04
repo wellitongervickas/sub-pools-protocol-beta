5d79257a37ebd0e8bf216bcb81f8202cf71c18d5
welliton gervickas <wellitogervickas@gmail.com>
2023-08-04 20:51:27 -0300

    fix: new proxies

--- contracts/core/strategy/StrategyProxy.sol
+++ contracts/core/strategy/StrategyProxy.sol
@@ -3,9 +3,13 @@ pragma solidity =0.8.19;
 
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {IStrategyProxy} from '../interfaces/strategy/IStrategyProxy.sol';
+import {StrategyProxyAdapter} from './StrategyProxyAdapter.sol';
+import {StrategyProxyAccounts} from './StrategyProxyAccounts.sol';
+import {StrategyProxyPositions} from './StrategyProxyPositions.sol';
+
 import {IStrategy} from '../interfaces/strategy/IStrategy.sol';
 
-contract StrategyProxy is IStrategyProxy, Ownable {
+contract StrategyProxy is IStrategyProxy, StrategyProxyAdapter, StrategyProxyAccounts, StrategyProxyPositions, Ownable {
     ///@inheritdoc IStrategyProxy
     IStrategy public immutable strategy;
 
