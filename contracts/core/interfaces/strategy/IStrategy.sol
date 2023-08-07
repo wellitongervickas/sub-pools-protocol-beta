293d6a1fbd4bd3f044056c801be19b0d1466b183
welliton gervickas <wellitogervickas@gmail.com>
2023-08-07 15:43:03 -0300

    fix: strategy

--- contracts/core/interfaces/strategy/IStrategy.sol
+++ contracts/core/interfaces/strategy/IStrategy.sol
@@ -2,6 +2,8 @@
 pragma solidity =0.8.19;
 
 interface IStrategy {
+    function strategist() external view returns (address);
+
     function deposit(address _depositor, bytes memory _amount) external returns (bytes memory);
 
     function withdraw(address _requisitor, bytes memory _amount) external returns (bytes memory);
--- contracts/test/FakeStrategy.sol
+++ contracts/test/FakeStrategy.sol
@@ -5,7 +5,11 @@ import {IStrategy} from '../core/interfaces/strategy/IStrategy.sol';
 import {EncodedERC20Transfer} from '../core/library/EncodedERC20Transfer.sol';
 
 contract FakeStrategy is IStrategy, EncodedERC20Transfer {
-    constructor(address[] memory assets_) EncodedERC20Transfer(assets_) {}
+    address public immutable strategist;
+
+    constructor(address[] memory assets_, address strategist_) EncodedERC20Transfer(assets_) {
+        strategist = strategist_;
+    }
 
     function deposit(address depositor_, bytes memory amount_) external returns (bytes memory) {
         return _deposit(depositor_, amount_);
