899459abdcbc11db6c9199283d5505f4c4c5d6a4
welliton gervickas <wellitogervickas@gmail.com>
2023-08-09 19:48:06 -0300

    fix: strategy fake

--- contracts/test/FakeStrategy.sol
+++ contracts/test/FakeStrategy.sol
@@ -5,12 +5,12 @@ import {ERC20Adapter} from '../core/modules/ERC20/ERC20Adapter.sol';
 import {BaseAdapter} from '../core/modules/adapters/BaseAdapter.sol';
 import {console} from 'hardhat/console.sol';
 
-contract FakeStrategy is BaseAdapter, ERC20Adapter {
+contract FakeStrategy is ERC20Adapter {
     constructor(address[] memory assets_) {
         assets = assets_;
     }
 
-    function deposit(address depositor_, uint256[] memory amount_) public override(BaseAdapter, ERC20Adapter) {
+    function deposit(address depositor_, uint256[] memory amount_) public override {
         return super.deposit(depositor_, amount_);
     }
 }
