1e51a1953bf66e11baf37b4181ef8d46caa7d053
welliton gervickas <wellitogervickas@gmail.com>
2023-08-19 20:21:51 -0300

    fix: emit node position created

--- test/node/position.spec.ts
+++ test/node/position.spec.ts
@@ -4,6 +4,24 @@ import { loadFixture, node } from '../fixtures'
 describe('Node', () => {
   describe('Position', () => {
     describe('Create', () => {
+      it('should emit Node_PositionCreated on create position', async function () {
+        const amount = '1000000000000000000'
+
+        const { nodeContract, accounts, tokenContract, vaultContract, vaultAddress, nodeAddress } = await loadFixture(
+          node.deployNodeFixture
+        )
+
+        const [depositor] = accounts
+
+        await tokenContract.approve(vaultAddress, amount)
+        await vaultContract.deposit(amount, depositor)
+        await vaultContract.approve(nodeAddress, amount)
+
+        await expect(nodeContract.createPosition([amount], depositor))
+          .to.emit(nodeContract, 'Node_PositionCreated')
+          .withArgs(1, depositor.address, [amount])
+      })
+
       it('should receive assets from vault', async function () {
         const amount = '1000000000000000000'
 
